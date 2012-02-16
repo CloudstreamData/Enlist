dead07a76540be2c05b236e961dad25c53df0ccd
Kurt Wiersma <kwiersma@gmail.com>
2012-02-15 22:08:52 -0600

    Initial commit of the mysql db setup script

---
+++ docs/sql/mysql_createDB.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `setting` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `defaultPointValue` int(11) DEFAULT NULL,
+  `orgName` varchar(100) DEFAULT NULL,
+  `orgDesc` text,
+  `orgAddress` varchar(100) DEFAULT NULL,
+  `sendEmail` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
